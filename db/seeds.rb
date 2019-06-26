3ca3969ce9f56556a8bd9e5f7b01ae67d4ea61e4
Alex Reed <ch.alexandre.reed@gmail.com>
2019-06-26 16:33:54 +0200

    api done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+names = %w(general paris react)
+nicknames = %w(Papillard ssaunier monsieurpaillard krokrob Eschults)
+
+channels = names.map do |name|
+  Channel.find_or_create_by(name: name)
+end
+
+users = nicknames.map do |nickname|
+  User.create(email: "#{nickname.downcase}@lewagon.com", nickname: nickname, password: "testtest")
+end
+
+40.times do
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::TvShows::HowIMetYourMother.quote
+end
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
+end
